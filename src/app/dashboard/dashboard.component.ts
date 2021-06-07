60ecf9b8a3a087329f5819f41e470cdc89ec11f9
Raghav <raghavgoel2701@gmail.com>
2021-06-07 12:08:41 +0530

    Added autocomplete to search

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -17,6 +17,43 @@ export class DashboardComponent implements OnInit {
     { title: 'Settings', url: '/Dashboard/Settings', icon: 'cog' },
   ];
   public labels = ['Family', 'Friends'];
+
+  searchResultsFound;
+  noSearchResultFound;
+  
+  allResults = [
+    {'name':'Dashboard', 'location':'./Home'},
+    {'name':'Leave', 'location':'./Leave'},
+    {'name':'Biometrics', 'location':'./Biometrics'},
+    {'name':'Time Sheet', 'location':'./Timesheet'},
+    {'name':'Holiday Calendar', 'location':'./Calendar'},
+    {'name':'Payroll', 'location':'./Payroll'},
+    {'name':'Settings', 'location':'./Settings'},
+  ];
+
+  searchResults = [];
+
+  autocomplete() : void {
+    this.searchResultsFound = false;
+    this.noSearchResultFound = false;
+
+    let search = document.getElementById('search') as HTMLIonInputElement;
+    let val = search.value as string;
+
+    if(val.trim() !== '') {
+      this.searchResults = this.allResults.filter((result) => {
+        return result.name.toLowerCase().indexOf(val.toLowerCase()) > -1;
+      });
+
+      if(this.searchResults.length > 0) {
+        this.searchResultsFound = true;
+      }
+      else {
+        this.noSearchResultFound = true;
+      }
+    }
+  }
+
   constructor(public authService:AuthService) { }
 
   ngOnInit() {}
