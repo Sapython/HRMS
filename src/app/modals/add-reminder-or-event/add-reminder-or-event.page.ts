1ed2d787edf39800743e613fbd65001c12f98d15
Raghav <raghavgoel2701@gmail.com>
2021-06-11 15:21:08 +0530

    Styled & validated modal

--- src/app/modals/add-reminder-or-event/add-reminder-or-event.page.ts
+++ src/app/modals/add-reminder-or-event/add-reminder-or-event.page.ts
@@ -7,21 +7,44 @@ import { ModalController, NavParams } from '@ionic/angular';
   styleUrls: ['./add-reminder-or-event.page.scss'],
 })
 export class AddReminderOrEventPage implements OnInit {
-  constructor(private modalController : ModalController, private navParams : NavParams) { }
+  date : string;
+  month : string;
+  year : string;
 
-  date; month; year;
+  title : string;
+  description : string;
+  wholeDay : boolean = true;
+  from : any;
+  to : any;
+
+  constructor(private modalController : ModalController, private navParams : NavParams) { }
 
   ngOnInit() {
     this.date = this.navParams.data.date;
     this.month = this.navParams.data.month;
     this.year = this.navParams.data.year;
+     
   }
 
   async add() {
-    //  Backend code for adding reminders HERE
+    if(this.isTitleValid()) {
+      //  Backend code for adding reminders HERE
+  
+      //  Close the modal
+      await this.modalController.dismiss();
+    }
 
-    //  Close the modal
-    await this.modalController.dismiss();
   }
 
+  isTitleValid() : boolean {
+    let titleInput = document.getElementById('title') as HTMLInputElement;
+    if(titleInput.value == '') {
+      titleInput.classList.add('error');
+      return false;
+    }
+    else {
+      titleInput.classList.remove('error');
+      return true;
+    }
+  }
 }
